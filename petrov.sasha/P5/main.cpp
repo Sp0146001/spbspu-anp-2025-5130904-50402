4516612be3c1e5a428564ef33c3515c33d1f73bf
sp0146001 <sp0146001@gmail.com>
2026-01-21 19:50:24 +0300

    rec_t + p_t + Shape class

--- petrov.sasha/P5/main.cpp
+++ petrov.sasha/P5/main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+namespace petrov
+{
+  struct point_t
+  {
+    double x;
+    double y;
+  };
 
+  struct rectangle_t
+  {
+    double width;
+    double height;
+    point_t pos;
+  };
+
+  class Shape
+  {
+  public:
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(const point_t& pos) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double k) = 0;
+  };
+}
 int main() {
   return 0;
 }
