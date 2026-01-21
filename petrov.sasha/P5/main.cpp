a0d24bbf2f75c80b8833f00bc9b3254d303f8847
sp0146001 <sp0146001@gmail.com>
2026-01-21 19:58:23 +0300

    Add Diamond(1 var)

--- petrov.sasha/P5/main.cpp
+++ petrov.sasha/P5/main.cpp
@@ -76,7 +76,63 @@ namespace petrov
     width_ *= k;
     height_ *= k;
   }
+  class Diamond : public Shape
+  {
+  public:
+    Diamond(const point_t& center, double diag_h, double diag_v);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(const point_t& pos) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
 
+  private:
+    point_t center_;
+    double diag_h_;
+    double diag_v_;
+  };
+
+  Diamond::Diamond(const point_t& center, double diag_h, double diag_v):
+    center_(center),
+    diag_h_(diag_h),
+    diag_v_(diag_v)
+  {
+    if (diag_h_ <= 0.0 || diag_v_ <= 0.0)
+    {
+      throw std::invalid_argument("Ошибка: диагонали должны быть положительными");
+    }
+  }
+
+  double Diamond::getArea() const
+  {
+    return (diag_h_ * diag_v_) / 2.0;
+  }
+
+  rectangle_t Diamond::getFrameRect() const
+  {
+    return {diag_h_, diag_v_, center_};
+  }
+
+  void Diamond::move(const point_t& pos)
+  {
+    center_ = pos;
+  }
+
+  void Diamond::move(double dx, double dy)
+  {
+    center_.x += dx;
+    center_.y += dy;
+  }
+
+  void Diamond::scale(double k)
+  {
+    if (k <= 0.0)
+    {
+      throw std::invalid_argument("Ошибка: коэффициент масштабирования должен быть положительным");
+    }
+    diag_h_ *= k;
+    diag_v_ *= k;
+  }
 }
 int main() {
   return 0;
